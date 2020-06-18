a190a2b46da7e1b5e35bf6bf573a87a02ed9387d
lucas-mateus <xlucasmat@gmail.com>
2020-06-18 17:17:18 -0300

    Impl árvore binária JS

---
+++ README.md
@@ -0,0 +1,2 @@
+# ArvoreBinJS
+Project intended to teach ***binary tree*** data structure in JavaScript for students undergraduate.
---
+++ js/binary_tree.js
@@ -0,0 +1,160 @@
+class BinaryTree {
+    // inicializa a raiz como nula
+    constructor() {
+        this.root = null
+    }
+
+    //exibe o menor valor da arvore
+    min() {
+        let current = this.root
+        if (current == null)
+            return null
+        while (current.left != null)
+            current = current.left
+        return current.content
+    }
+
+    //exibe o maior valor da arvore
+    max() {
+        let current = this.root
+        if (current == null)
+            return null
+        while (current.right != null)
+            current = current.right
+        return current.content
+    }
+
+    //insere o elemento da arvores
+    insert(element) {
+        this.root = this.insertNode(this.root, element)
+    }
+
+    insertNode(rootNode, element) {
+        if (rootNode == null)
+            return new Node(element)
+        if (element > rootNode.content)
+            rootNode.right = this.insertNode(rootNode.right, element)
+        else
+            rootNode.left = this.insertNode(rootNode.left, element)
+        return rootNode
+    }
+
+    //executa a função callback para cada nó, em ordem
+    inOrderTraverse(callback) {
+        this.inOrderVisitor(this.root, callback)
+    }
+
+    inOrderVisitor(node, callback) {
+        if (node == null)
+            return
+        this.inOrderVisitor(node.left, callback)
+        callback(node.content)
+        this.inOrderVisitor(node.right, callback)
+    }
+
+    //executa a função callback para cada nó, em pré-ordem
+    preOrderTraverse(callback) {
+        this.preOrderVisitor(this.root, callback)
+    }
+
+    preOrderVisitor(node, callback) {
+        if (node == null)
+            return
+        callback(node.content)
+        this.preOrderVisitor(node.left, callback)
+        this.preOrderVisitor(node.right, callback)
+    }
+
+    //executa a função callback para cada nó, em pós-ordem
+    postOrderTraverse(callback) {
+        this.postOrderVisitor(this.root, callback)
+    }
+
+    postOrderVisitor(node, callback) {
+        if (node == null)
+            return
+        this.postOrderVisitor(node.left, callback)
+        this.postOrderVisitor(node.right, callback)
+        callback(node.content)
+    }
+
+    //retorna true se o valor já existe na arvore 
+    //     Busca na árvore binária
+    //    1. É nulo? o elemento não existe
+    //    2. É igual ao conteúdo? achou
+    //    3. É maior que o conteúdo?
+    //       3.1 busca de direita
+    //       3.2 busca na esquerda
+
+    search(value) {
+        return this.searchVisitor(this.root, value)
+    }
+
+    searchVisitor(node, element) {
+        if (node == null)
+            return false
+        if (node.content == element)
+            return true;
+        if (element > node.content)
+            return this.searchVisitor(node.right, element)
+        else
+            return this.searchVisitor(node.left, element)
+    }
+
+    //remove um elemento existente na arvore o retorna
+    remove(value) {
+        this.root = this.removeVisitor(this.root, value)
+    }
+
+    removeVisitor(node, value) {
+        if (node.content == value) {
+            if (node.left == node.right) {
+                //nao tem filhos - Grau 0
+                return null
+            } else if (node.right == null) {
+                //não tem filhos na direita, e tem nó na esqueda - Grau 1
+                return node.left
+            } else if (node.left == null) {
+                //não tem filhos da esquerda, e tem nó da direita - Grau 1
+                return node.right
+            } else {
+                // tem os dois ramos - Grau 2
+                const newRoot = node.right
+                let current = node.right;
+                while (current.left != null)
+                    current = current.left
+                current.left = node.left
+                return newRoot;
+            }
+        } else if (value < node.content) {
+            node.left = this.removeVisitor(node.left, value)
+        } else {
+            node.right = this.removeVisitor(node.right, value)
+        }
+        return node;
+    }
+
+    //exibe a altura da arvore
+    height() {
+        return this.heightVisitor(this.root)
+    }
+
+    heightVisitor(node) {
+        if (!node)
+            return -1
+        let leftHeight = this.heightVisitor(node.left),
+            rightHeight = this.heightVisitor(node.right)
+        return Math.max(leftHeight, rightHeight) + 1
+    }
+
+    // informa quantos nós existem na arvore
+    size() {
+        return this.sizeVisitor(this.root)
+    }
+
+    sizeVisitor(node) {
+        if (!node)
+            return 0
+        return this.sizeVisitor(node.left) + this.sizeVisitor(node.right) + 1
+    }
+}
